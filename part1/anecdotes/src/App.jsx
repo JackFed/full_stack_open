3aa8ce2b4503205d8fb8634c414c87191dc1092e
JackFed <jack.fedurek@gmail.com>
2025-02-14 13:07:27 -0500

    Finish exercise 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -11,19 +11,53 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+  
+  const initialVotes = Array(anecdotes.length).fill(0)
+
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(initialVotes)
 
   const findAnecdote = () => {
-    console.log(anecdotes.length)
     const randNum = Math.floor(Math.random() * anecdotes.length)
     setSelected(randNum)
   }
 
+  const vote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
+  const mostVoted = () => {
+    let maxIndex = 0
+    let maxValue = -1
+    for (let i = 0; i < votes.length; i++) {
+      console.log(votes[i])
+      console.log(maxValue)
+      if (votes[i] > maxValue) {
+        maxIndex = i
+        maxValue = votes[i]
+      }
+    }
+    return maxIndex
+  }
+
+  const mostVotes = mostVoted()
+
   return (
     <div>
-      {anecdotes[selected]}
-      <button onClick={findAnecdote}>Get Random Anecdote</button>
+      <div>
+        <h1>Anecdote of the day</h1>
+        <p>{anecdotes[selected]}</p>
+        <p>Votes: {votes[selected]}</p>
+        <button onClick={vote}>Vote</button>
+        <button onClick={findAnecdote}>Get Random Anecdote</button>
+      </div>
+      <div>
+        <h1>Anecdote with most votes</h1>
+        <p>{anecdotes[mostVotes]}</p>
+        <p>Votes: {votes[mostVotes]}</p>
+      </div>
     </div>
   )
 }
