46e07d437f666eb2508f6036750048dad70f73b1
JackFed <jack.fedurek@gmail.com>
2025-02-25 15:48:11 -0500

    Make model for person db, edit api get to get people from db

--- part3/phonebook/phonebook-backend/index.js
+++ part3/phonebook/phonebook-backend/index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express() 
 const morgan = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(cors())
@@ -38,7 +40,9 @@ let persons = [
 ]
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons=> {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
@@ -94,7 +98,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
