43efb3b1898c554b231ce62d21924584dee1d98b
JackFed <jack.fedurek@gmail.com>
2025-02-26 09:20:35 -0500

    Finish exercise 3.17, add put call for changing number

--- part3/phonebook/phonebook-backend/index.js
+++ part3/phonebook/phonebook-backend/index.js
@@ -109,6 +109,21 @@ app.post('/api/persons', (request, response, next) => {
     }).catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, body, {new:true})
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
 app.use(errorHandler)
 
 const PORT = process.env.PORT
