5a13a6d2dac4e08dc7d87d854eff48efffbbc0d7
JackFed <jack.fedurek@gmail.com>
2025-02-26 08:57:59 -0500

    Remove unused code and connect all api calls to db

--- part3/notes/notes-backend/index.js
+++ part3/notes/notes-backend/index.js
@@ -19,23 +19,15 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -47,7 +39,7 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id)
         .then(note => {
             if (note) {
@@ -57,8 +49,7 @@ app.get('/api/notes/:id', (request, response) => {
             }
         })
         .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: 'malformatted id'})
+            next(error)
         })
 })
 
@@ -70,16 +61,23 @@ app.delete('/api/notes/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0 
-        ? Math.max(...notes.map(n => Number(n.id))) 
-        : 0
-    return String(maxId + 1)
-}
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+    const note = {
+        content: body.content,
+        important: body.important
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
 
 app.post('/api/notes', (request, response) => { 
     const body = request.body
-
+    
     if (!body.content) {
         return response.status(400).json({
             error: 'content missing'
@@ -102,6 +100,9 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
