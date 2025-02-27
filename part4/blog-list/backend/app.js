ec87f80a9aae0c80dbd56b9cae2b4d86ec336f00
JackFed <jack.fedurek@gmail.com>
2025-02-27 14:01:04 -0500

    Refactor index.js and add app.js for express functions

--- part4/blog-list/backend/app.js
+++ part4/blog-list/backend/app.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const blogRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
+const { MONGODB_URI } = require('./utils/config')
 
-mongoose.set('strictQuery', false)
-
-logger.info(`Connecting to db: ${url}`)
-mongoose.connect(url)
+logger.info(`Connecting to db: ${MONGODB_URI}`)
+mongoose.connect(MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
   })
@@ -16,8 +15,8 @@ mongoose.connect(url)
     logger.error('error connecting to MongoDB:', error.message)
   })
 
-app.use('cors')
-
+app.use(cors())
+app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
 
--- part4/blog-list/backend/index.js
+++ part4/blog-list/backend/index.js
@@ -1,25 +1,6 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
-const Blog = require('./models/blog')
-const blogRouter = require('./controllers/blogs')
+const app = require('./app')
 const logger = require('./utils/logger')
-const { PORT, MONGODB_URI } = require('./utils/config')
-
-logger.info(`Connecting to db: ${MONGODB_URI}`)
-mongoose.connect(MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogRouter)
+const { PORT } = require('./utils/config')
 
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
