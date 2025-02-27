200bbe2723ba9c84fad5493415de16e85d922654
JackFed <jack.fedurek@gmail.com>
2025-02-27 14:06:21 -0500

    Finish exercise 4.2: Add middleware utility logic and incorperate with app.js

--- part4/blog-list/backend/app.js
+++ part4/blog-list/backend/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const { MONGODB_URI } = require('./utils/config')
+const middleware = require('./utils/middleware')
 
 logger.info(`Connecting to db: ${MONGODB_URI}`)
 mongoose.connect(MONGODB_URI)
@@ -17,8 +18,12 @@ mongoose.connect(MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
