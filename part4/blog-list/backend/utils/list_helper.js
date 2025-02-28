6de08f43ff0e461ecfd7cab6f2832f12b95c5cc5
JackFed <jack.fedurek@gmail.com>
2025-02-28 11:49:34 -0500

    Finish exercise 4.5

--- part4/blog-list/backend/utils/list_helper.js
+++ part4/blog-list/backend/utils/list_helper.js
@@ -8,7 +8,14 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  return blogs.reduce((max, blog) => (blog.likes >= max.likes ? blog : max), blogs[0])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
