3c5f28499ffadf60872495d3dcdd313293d3f58b
JackFed <jack.fedurek@gmail.com>
2025-03-05 09:16:15 -0500

    Create GET user api to see all users

--- part4/blog-list/backend/controllers/blogs.js
+++ part4/blog-list/backend/controllers/blogs.js
@@ -1,12 +1,12 @@
-const blogRouter = require('express').Router()
+const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', async (request, response) => {
+blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs)
 })
 
-blogRouter.post('/', async (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
   const savedBlog = await blog.save()
@@ -14,16 +14,16 @@ blogRouter.post('/', async (request, response) => {
     
 })
 
-blogRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndDelete(request.params.id)
   response.status(204).end()
 })
 
-blogRouter.put('/:id', async (request, response) => {
+blogsRouter.put('/:id', async (request, response) => {
   const { title, author, url, likes } = request.body
 
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, { title, author, url, likes }, {new: true})
   response.json(updatedBlog)
 })
 
-module.exports = blogRouter
+module.exports = blogsRouter
--- part4/blog-list/backend/controllers/users.js
+++ part4/blog-list/backend/controllers/users.js
@@ -2,6 +2,11 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
 usersRouter.post('/', async (request, response) => {
   const { name, username, password } = request.body
 
