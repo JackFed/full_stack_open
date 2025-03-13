9f5afbb0d276595199ee56083ff6ad425b984d40
JackFed <jack.fedurek@gmail.com>
2025-03-13 12:12:48 -0400

    Add login api call to login handler

--- part5/blog-list/bloglist-frontend/src/App.jsx
+++ part5/blog-list/bloglist-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 import LoginForm from './components/Login'
 
 const App = () => {
@@ -15,9 +16,24 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
     console.log(`Username ${username}, password ${password}`)
+    try {
+      const loginData = await loginService.login({
+        username,
+        password
+      })
+      setUser(loginData)
+      setUsername('')
+      setPassword('')
+      console.log(loginData)
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
 
   return (
@@ -27,6 +43,7 @@ const App = () => {
         : 
           <>
             <h2>blogs</h2>
+            <p>{user.name} is logged in</p>
             {blogs.map(blog =>
               <Blog key={blog.id} blog={blog} />
             )}
