b8726070285a85e045204263b4cd1fe6dcaa0677
JackFed <jack.fedurek@gmail.com>
2025-03-17 15:32:19 -0400

    Finish 5.7: Add view more option to blog components

--- part5/blog-list/bloglist-frontend/src/components/Blog.jsx
+++ part5/blog-list/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [shown, setShown] = useState(false)
+
+  const toggleShown = () => setShown(!shown)
+
+  return ( 
+    <div style={blogStyle}>
+      {blog.title} {blog.author} 
+      <button onClick={toggleShown}>
+        { shown ? 'hide' : 'view'}
+      </button>
+      {
+        shown && 
+        <div>
+          <div>{blog.url}</div>
+          <div>likes: {blog.likes}</div>
+          <div>creator: {blog.user.username}</div>
+        </div>
+      }
+    </div>  
+  )
+}
 
 export default Blog
